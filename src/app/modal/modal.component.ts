4bc5dd2fbd4af732a6c2041717262bcb7d0a9af9
peejay <peejay.beltran12@gmail.com>
2024-03-27 19:19:21 +0800

    seventh commit

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -8,12 +8,37 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalComponent  implements OnInit {
   condition = false;
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private alertController: AlertController, private toastController: ToastController) { }
 
   ngOnInit() {}
 
   async closeModal(){
     await this.modalController.dismiss();
+  }
+
+  async toggleModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent
+    });
+    return await modal.present();
+  }
+
+  async alert(){
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async presentToast(){
+    const toast = await this.toastController.create({
+      message: 'Your Toast Message Here',
+      duration: 2000
+    });
+    toast.present();
   }
 
 
